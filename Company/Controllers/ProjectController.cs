02194a4515ca68c7097f4c5cdec1b3a1a0e422d3
Grellinn <grellinn@gmail.com>
2014-07-06 01:59:19 +0000

    Project.cs validation fix, ProjectController og Views re-scaffolded

--- Company/Controllers/ProjectController.cs
+++ Company/Controllers/ProjectController.cs
@@ -8,209 +8,126 @@ using System.Web;
 using System.Web.Mvc;
 using Company.Models;
 using Company.DAL;
-using Company.Repositories;
-using PagedList;
-using Company.ViewModels;
 
 namespace Company.Controllers
 {
-	public class ProjectController : Controller
-	{
-		private IProjectRepository projectRepo;
+    public class ProjectController : Controller
+    {
+        private CompanyContext db = new CompanyContext();
 
-		public ProjectController()
-		{
-			this.projectRepo = new ProjectRepository(new CompanyContext());
-		}
+        // GET: /Project/
+        public ActionResult Index()
+        {
+            var projects = db.Projects.Include(p => p.Client);
+            return View(projects.ToList());
+        }
 
-		// GET: /Project/
-		public ActionResult Index(string sortOrder, string searchString, string currentFilter, int? page)
-		{
-			var projects = projectRepo.GetProjects();
+        // GET: /Project/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
 
-			#region leitarvél
-			if (!String.IsNullOrEmpty(searchString))
-			{
-				projects = projects.Where(p => p.Title.ToUpper().Contains(searchString.ToUpper()) || p.Title.ToUpper().Contains(searchString.ToUpper())).ToList();
-			}
-			#endregion
+        // GET: /Project/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientID = new SelectList(db.Clients, "ID", "Name");
+            return View();
+        }
 
-			#region ViewBags
-			ViewBag.CurrentSort = sortOrder;
-			ViewBag.TitleSortParm = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
-			ViewBag.RegisteredSortParm = sortOrder == "Registered" ? "registered_desc" : "Registered";
-			ViewBag.StatusSortParm = sortOrder == "Status" ? "status_desc" : "Status";
-			ViewBag.ClientSortParm = sortOrder == "Client" ? "client_desc" : "Client";
-			ViewBag.AddressSortParm = sortOrder == "Address" ? "address_desc" : "Address";
-			ViewBag.PhoneSortParm = sortOrder == "Phone" ? "phone_desc" : "Phone";
+        // POST: /Project/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Title,Address,ZipCode,PhotoPath,ClientID,Details,OfferPrice,HourPrice,TotalPrice,NumberOfHours,Status,RegisteredDate,FinishDate")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-			if (searchString != null)
-			{
-				page = 1;
-			}
-			else
-			{
-				searchString = currentFilter;
-			}
+            ViewBag.ClientID = new SelectList(db.Clients, "ID", "Name", project.ClientID);
+            return View(project);
+        }
 
-			ViewBag.CurrentFilter = searchString;
-			#endregion
+        // GET: /Project/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientID = new SelectList(db.Clients, "ID", "Name", project.ClientID);
+            return View(project);
+        }
 
-			#region switch for sortOrder
-			switch (sortOrder)
-			{
-				case "title_desc":
-					projects = projects.OrderByDescending(p => p.Title).ToList();
-					break;
-				case "Registered":
-					projects = projects.OrderBy(p => p.RegisteredDate).ToList();
-					break;
-				case "registered_desc":
-					projects = projects.OrderByDescending(p => p.RegisteredDate).ToList();
-					break;
-				case "Status":
-					projects = projects.OrderBy(p => p.Status).ToList();
-					break;
-				case "status_desc":
-					projects = projects.OrderByDescending(p => p.Status).ToList();
-					break;
-				case "Client":
-					projects = projects.OrderBy(p => p.Client.Name).ToList();
-					break;
-				case "client_desc":
-					projects = projects.OrderByDescending(p => p.Client.Name).ToList();
-					break;
-				case "Address":
-					projects = projects.OrderBy(c => c.Address).ToList();
-					break;
-				case "address_desc":
-					projects = projects.OrderByDescending(c => c.Address).ToList();
-					break;
-				case "Phone":
-					projects = projects.OrderBy(p => p.Client.Phone).ToList();
-					break;
-				case "phone_desc":
-					projects = projects.OrderByDescending(p => p.Client.Phone).ToList();
-					break;
-				default:
-					projects = projects.OrderBy(p => p.Title).ToList();
-					break;
-			}
-			#endregion
+        // POST: /Project/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Title,Address,ZipCode,PhotoPath,ClientID,Details,OfferPrice,HourPrice,TotalPrice,NumberOfHours,Status,RegisteredDate,FinishDate")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientID = new SelectList(db.Clients, "ID", "Name", project.ClientID);
+            return View(project);
+        }
 
-			int pageSize = 10;
-			int pageNumber = (page ?? 1);
-			return View(projects.ToPagedList(pageNumber, pageSize));
-		}
+        // GET: /Project/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
 
-		// GET: /Project/Details/5
-		public ActionResult Details(int id)
-		{
-			Project project = projectRepo.GetProjectByID(id);
-			return View(project);
-		}
+        // POST: /Project/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Project project = db.Projects.Find(id);
+            db.Projects.Remove(project);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-		// GET: /Project/Create
-		public ActionResult Create()
-		{
-			ProjectVM viewModel = new ProjectVM();
-			return View(viewModel);
-		}
-
-		// POST: /Project/Create
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Create([Bind(Include = "Project, Project.Client, Clients, StatusList")] ProjectVM viewModel)
-		{
-			Project projectToAdd = projectRepo.CreateProjectToInsert(viewModel.Project);
-			projectToAdd.Client = new Client();
-			projectToAdd.Client.ID = viewModel.Project.Client.ID;
-
-			#region check for errors on Client
-			if (ModelState["Project.Client.Name"].Errors.Count == 1)
-			{
-				ModelState["Project.Client.Name"].Errors.Clear();
-			}
-			if (ModelState["Project.Client.Address"].Errors.Count == 1)
-			{
-				ModelState["Project.Client.Address"].Errors.Clear();
-			}
-			if (ModelState["Project.Client.Email"].Errors.Count == 1)
-			{
-				ModelState["Project.Client.Email"].Errors.Clear();
-			}
-			#endregion
-
-			if (ModelState.IsValid)
-			{
-				projectRepo.InsertProject(projectToAdd);
-				projectRepo.Save();
-				return RedirectToAction("Index");
-			}
-
-			return View(viewModel.Project);
-		}
-
-		// GET: /Project/Edit/5
-		public ActionResult Edit(int id)
-		{
-			ProjectVM viewModel = new ProjectVM();
-			viewModel.Project = projectRepo.GetProjectByID(id);
-			return View(viewModel);
-		}
-
-		// POST: /Project/Edit/5
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit([Bind(Include = "Project, Project.Client, Clients, StatusList")] ProjectVM viewModel)
-		{
-			/*ProjectVM tempVM = new ProjectVM();
-			viewModel.Project.Client = tempVM.Clients.Where(c => c.ID == viewModel.Project.Client.ID).SingleOrDefault();*/
-
-			#region check for errors on Client
-			if (ModelState["Project.Client.Name"].Errors.Count == 1)
-			{
-				ModelState["Project.Client.Name"].Errors.Clear();
-			}
-			if (ModelState["Project.Client.Address"].Errors.Count == 1)
-			{
-				ModelState["Project.Client.Address"].Errors.Clear();
-			}
-			if (ModelState["Project.Client.Email"].Errors.Count == 1)
-			{
-				ModelState["Project.Client.Email"].Errors.Clear();
-			}
-			#endregion
-
-			if (ModelState.IsValid)
-			{
-				projectRepo.UpdateProject(viewModel.Project);
-				projectRepo.Save();
-				return RedirectToAction("Index");
-			}
-			return View(viewModel);
-		}
-
-		// GET: /Project/Delete/5
-		public ActionResult Delete(int id)
-		{
-			Project project = projectRepo.GetProjectByID(id);
-			return View(project);
-		}
-
-		// POST: /Project/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public ActionResult DeleteConfirmed(int id)
-		{
-			Project project = projectRepo.GetProjectByID(id);
-			projectRepo.DeleteProject(id);
-			projectRepo.Save();
-			return RedirectToAction("Index");
-		}
-	}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
 }
--- Company/Models/Project.cs
+++ Company/Models/Project.cs
@@ -27,34 +27,34 @@ namespace Company.Models
 		public string PhotoPath { get; set; }
 
 		[Display(Name = "Viðskiptavinur")]
-		[Required(ErrorMessage = "Veldu viðskiptavin")]
+		[Required(ErrorMessage="Veldu viðskiptavin")]
 		public int ClientID { get; set; }
 
 		[Display(Name = "Upplýsingar")]
 		public string Details { get; set; }
 
 		[Display(Name = "Tilboðsverð")]
-		public int OfferPrice { get; set; }
+		public int? OfferPrice { get; set; }
 
 		[Display(Name = "Tímaverð")]
-		public int HourPrice { get; set; }
+		public int? HourPrice { get; set; }
 		#endregion
 
 		#region Properties that the system uses
 		[Display(Name = "Heildarverð")]
-		public int TotalPrice { get; set; }
+		public int? TotalPrice { get; set; }
 
 		[Display(Name = "Heildarfjöldi tíma")]
-		public int NumberOfHours { get; set; }
+		public int? NumberOfHours { get; set; }
 		
 		[Display(Name = "Staða")]
 		public string Status { get; set; }
 
 		[Display(Name = "Skráð")]
-		public DateTime RegisteredDate { get; set; }
+		public DateTime? RegisteredDate { get; set; }
 
 		[Display(Name = "Lokið þann")]
-		public DateTime FinishDate { get; set; }
+		public DateTime? FinishDate { get; set; }
 		#endregion
 
 		[Display(Name = "Viðskiptavinur")]
