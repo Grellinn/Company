8599f958a5210738b6e807f5f6f689552c095651
Grellinn <grellinn@gmail.com>
2014-07-08 19:28:27 +0000

    Panel fix á nokkrum Views

--- Company/DAL/CompanyInitializer.cs
+++ Company/DAL/CompanyInitializer.cs
@@ -28,7 +28,7 @@ namespace Company.DAL
 				new Project { Title="Mála stein á framhlið", ClientID=1, Details ="Mála allan stein að framan með málara hvítu", StartedDate=DateTime.Parse("2014/07/06"), OfferPrice=0, TotalIncome=0, TotalExpense=0, Address="Hátún 6B", ZipCode="105 RVK", Status="Verkefni hafið", RegisteredDate=DateTime.Now },
 				new Project { Title="Mála tréverk á framhlið", ClientID=1, Details ="Mála allt tréverk að framan með toskana gráu", OfferPrice=0, TotalIncome=0, TotalExpense=0, Address="Hátún 6B", ZipCode="105 RVK", Status="Ekki hafið", RegisteredDate=DateTime.Now },
 				new Project { Title="Gera við og sprauta þak", ClientID=1, Details ="Þvo, gera við og sprauta þak með dökk gráu", StartedDate=DateTime.Parse("2014/07/06"), OfferPrice=0, TotalIncome=1200000, TotalExpense=240000, Address="Hátún 6B", ZipCode="105 RVK", Status="Verkefni lokið", RegisteredDate=DateTime.Now, FinishDate=DateTime.Parse("2014/07/10"), NumberOfHours=4 },
-				new Project { Title="Gera við og sprauta bílskúrsþak", ClientID=1, Details ="Þvo, gera við og sprauta bílskúrsþak með dökk gráu", StartedDate=DateTime.Parse("2014/07/06"), OfferPrice=0, TotalIncome=0, TotalExpense=0, Address="Hátún 6B", ZipCode="105 RVK", Status="Verkefni lokið", RegisteredDate=DateTime.Now, FinishDate=DateTime.Parse("2014/07/10"), NumberOfHours=4 },
+				new Project { Title="Gera við og sprauta bílskúrsþak", ClientID=1, Details ="Þvo, gera við og sprauta bílskúrsþak með dökk gráu", StartedDate=DateTime.Parse("2014/07/06"), OfferPrice=0, TotalIncome=0, TotalExpense=0, Address="Hátún 6B", ZipCode="105 RVK", Status="Verkefni hafið", RegisteredDate=DateTime.Now },
 				new Project { Title="Mála stein á bílskúr", ClientID=1, Details ="Mála allan stein á bílskúr með málara hvítu", StartedDate=DateTime.Parse("2014/07/06"), OfferPrice=0, TotalIncome=0, Address="Hátún 6B", TotalExpense=0, ZipCode="105 RVK", Status="Verkefni hafið", RegisteredDate=DateTime.Now },
 				new Project { Title="Mála tréverk á bílskúr", ClientID=1, Details ="Mála allt tréverk á bílskúr með toskana gráu", OfferPrice=0, TotalIncome=0, Address="Hátún 6B", ZipCode="105 RVK", Status="Ekki hafið", TotalExpense=0, RegisteredDate=DateTime.Now }
 			};
@@ -38,12 +38,12 @@ namespace Company.DAL
 
 			var incomes = new List<Income>
 			{
-				new Income { Description="Fyrsti hluti fyrir framkvæmdum á húsi", ProjectID=1, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
-				new Income { Description="Annar hluti fyrir framkvæmdum á húsi", ProjectID=1, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
-				new Income { Description="Þriðji hluti fyrir framkvæmdum á húsi", ProjectID=1, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
-				new Income { Description="Fyrsti hluti fyrir framkvæmdum á bílskúr", ProjectID=1, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
-				new Income { Description="Annar hluti fyrir framkvæmdum á bílskúr", ProjectID=1, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
-				new Income { Description="Þriðji hluti fyrir framkvæmdum á bílskúr", ProjectID=1, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 }
+				new Income { Description="Fyrsti hluti fyrir framkvæmdum á húsi", ProjectID=3, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
+				new Income { Description="Annar hluti fyrir framkvæmdum á húsi", ProjectID=3, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
+				new Income { Description="Þriðji hluti fyrir framkvæmdum á húsi", ProjectID=3, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
+				new Income { Description="Fyrsti hluti fyrir framkvæmdum á bílskúr", ProjectID=3, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
+				new Income { Description="Annar hluti fyrir framkvæmdum á bílskúr", ProjectID=3, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 },
+				new Income { Description="Þriðji hluti fyrir framkvæmdum á bílskúr", ProjectID=3, Registered=DateTime.Now, Title="Innborgun frá Hátúni 6B", Amount=200000 }
 			};
 
 			incomes.ForEach(i => dbContext.Incomes.Add(i));
@@ -51,12 +51,12 @@ namespace Company.DAL
 
 			var expenses = new List<Expense>
 			{
-				new Expense { Title="Málning ehf.", Amount=40000, Description="Málara hvítt Steintex á stein 100L", ProjectID=1, Registered=DateTime.Now },
-				new Expense { Title="Málning ehf.", Amount=40000, Description="Toskana grár Kjörvari á tré 20L", ProjectID=1, Registered=DateTime.Now },
-				new Expense { Title="Málning ehf.", Amount=40000, Description="Dökk grá þakmálning 40L", ProjectID=1, Registered=DateTime.Now },
-				new Expense { Title="Launakostnaður", Amount=40000, Description="40 vinnustundir, Grétar, Davíð og Davíð", ProjectID=1, Registered=DateTime.Now },
-				new Expense { Title="Launakostnaður", Amount=40000, Description="40 vinnustundir, Grétar, Davíð og Davíð", ProjectID=1, Registered=DateTime.Now },
-				new Expense { Title="Launakostnaður", Amount=40000, Description="30 vinnustundir, Grétar, Davíð og Davíð", ProjectID=1, Registered=DateTime.Now }
+				new Expense { Title="Málning ehf.", Amount=40000, Description="Málara hvítt Steintex á stein 100L", ProjectID=3, Registered=DateTime.Now },
+				new Expense { Title="Málning ehf.", Amount=40000, Description="Toskana grár Kjörvari á tré 20L", ProjectID=3, Registered=DateTime.Now },
+				new Expense { Title="Málning ehf.", Amount=40000, Description="Dökk grá þakmálning 40L", ProjectID=3, Registered=DateTime.Now },
+				new Expense { Title="Launakostnaður", Amount=40000, Description="40 vinnustundir, Grétar, Davíð og Davíð", ProjectID=3, Registered=DateTime.Now },
+				new Expense { Title="Launakostnaður", Amount=40000, Description="40 vinnustundir, Grétar, Davíð og Davíð", ProjectID=3, Registered=DateTime.Now },
+				new Expense { Title="Launakostnaður", Amount=40000, Description="30 vinnustundir, Grétar, Davíð og Davíð", ProjectID=3, Registered=DateTime.Now }
 			};
 
 			expenses.ForEach(e => dbContext.Expenses.Add(e));
