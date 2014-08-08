02b09205678e273aaead0d4208ef712ec0ee9aed
Grellinn <grellinn@gmail.com>
2014-08-08 17:10:50 +0000

    Employee og Hour model búin til

---
+++ Company/Models/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Company.Models
+{
+	public class Employee
+	{
+		public int ID { get; set; }
+
+		[Display(Name = "Nafn")]
+		[Required(ErrorMessage = "Vinsamlegast sláðu inn nafn")] // Verður að taka gildi, gefur annars villumeldingu
+		[StringLength(60, MinimumLength = 3)]
+		public string Name { get; set; }
+
+		[Display(Name = "Heimilisfang")]
+		[StringLength(60, MinimumLength = 3)]
+		public string Address { get; set; }
+
+		[Display(Name = "Upplýsingar")]
+		public string Details { get; set; }
+
+		[Display(Name = "Kennitala")]
+		public string SSN { get; set; }
+
+		[Display(Name = "Bankareikningsnr.")]
+		public string AccountNr { get; set; }
+
+		[Display(Name = "Sími")]
+		[Required(ErrorMessage = "Vinsamlegast sláðu inn símanúmer")] // Verður að taka gildi, gefur annars villumeldingu
+		public string Phone { get; set; }
+
+		[Display(Name = "Tölvupóstur")]
+		[EmailAddress]
+		[StringLength(60, MinimumLength = 3)]
+		public string Email { get; set; }
+
+		[Display(Name = "Staða innan fyrirtækis")]
+		public string Role { get; set; }
+
+		[Display(Name = "Hóf störf")]
+		public DateTime Started { get; set; }
+
+		[Display(Name = "Lauk störfum")]
+		public DateTime Quit { get; set; }
+
+		[Display(Name = "Tímakaup")]
+		public int WPH { get; set; }
+
+		[Display(Name = "Verkefni skráð á starfsmann")]
+		public virtual ICollection<Project> Projects { get; set; }
+		
+		[Display(Name = "Tímar skráðir á starfsmann")]
+		public virtual ICollection<Hour> Hours { get; set; }
+	}
+}
---
+++ Company/Models/Hour.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Company.Models
+{
+	public class Hour
+	{
+		public int ID { get; set; }
+
+		[Display(Name = "Starfsmaður")]
+		[Required(ErrorMessage = "Veldu starfsmann")]
+		public int EmployeeID { get; set; }
+
+		[Display(Name = "Verkefni")]
+		[Required(ErrorMessage = "Veldu verkefni")]
+		public int ProjectID { get; set; }
+
+		[Display(Name = "Stimpla inn í byrjun vinnudags")]
+		public DateTime DayStart { get; set; }
+
+		[Display(Name = "Stimpla út í morgunkaffi")]
+		public DateTime MorgningBreakStop { get; set; }
+
+		[Display(Name = "Stimpla inn eftir morgunkaffi")]
+		public DateTime MorgningBreakStart { get; set; }
+
+		[Display(Name = "Stimpla út í hádegismat")]
+		public DateTime LunchStop { get; set; }
+
+		[Display(Name = "Stimpla inn eftir hádegismat")]
+		public DateTime LunchStart { get; set; }
+
+		[Display(Name = "Stimpla út í síðdegiskaffi")]
+		public DateTime DayBreakStop { get; set; }
+
+		[Display(Name = "Stimpla inn eftir síðdegiskaffi")]
+		public DateTime DayBreakStart { get; set; }
+
+		[Display(Name = "Stimpla út í lok vinnnudags")]
+		public DateTime DayStop { get; set; }
+
+		[Display(Name = "Dagsetning stimplunar")]
+		public DateTime Date { get; set; }
+
+		[Display(Name = "Heildarfjöldi tíma dags")]
+		public double WorkTime { get; set; }
+
+
+		[Display(Name = "Verkefni")]
+		public virtual Project Project { get; set; }
+
+		[Display(Name = "Employee")]
+		public virtual Employee Employee { get; set; }
+	}
+}
